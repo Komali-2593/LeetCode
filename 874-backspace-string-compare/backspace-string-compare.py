24ab7367f31fa8efc07665ac025df0a193154c77
SIDDHANI KOMALI ALEKHYA <komalialekhyas@gmail.com>
2026-06-08 16:49:22 +0530

    Time: 4 ms (4.22%) | Memory: 19.5 MB (5.39%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.py
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -4,17 +4,15 @@ class Solution:
         self.stack2=[]
         for char in s:
             if char=="#":
-                if not self.stack1:
-                    self.stack1.append(char)
+                if  self.stack1:
+                    self.stack1.pop()
                     
-                self.stack1.pop()
             else:
                 self.stack1.append(char)
         for char in t:
             if char=="#":
-                if not self.stack2:
-                    self.stack2.append(char)
-                self.stack2.pop()
+                if self.stack2:
+                    self.stack2.pop()
             else:
                 self.stack2.append(char)
         return self.stack1==self.stack2
